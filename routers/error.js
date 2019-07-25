00cc8fbef34d85b12fc2ca939433325f14e05811
HyunMin An <dksgusals99@gmail.com>
2019-07-25 12:57:11 +0900

    1

--- routers/error.js
+++ routers/error.js
@@ -2,13 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 router.use(function (req, res, next) {
-    
-    console.log(req);
-    res.status(404).send('Sorry cant find that!');
+    var path = req.headers.host + req.url
+    res.status(404).send(`Sorry cant find ${path}!`);
+    //next();
 });
 
 router.use(function (err, req, res, next) {
     console.log(err);
+    //next();
 });
 
 module.exports = router;
--- routers/index.js
+++ routers/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.send('Router Test Index');
-    next();
+    //next();
 });
 
 module.exports = router;
