226899d3f520e1b45e9a3a0ea326634cf5b0efdb
unknown <dksgusals99@gmail.com>
2019-07-26 13:33:41 +0900

    1

--- cluster.js
+++ cluster.js
@@ -1,17 +1,14 @@
 const os = require('os');
 const cluster = require('cluster');
-const cpuNum = os.cpus.length;
+const cpuNum = os.cpus().length;
 
 if (cluster.isMaster) {
+    console.log("Product cluster.js");
     //cpu 개수만큼 프로세스 생성
-    for (var i=0;i<cpuNum;++i) {
-        cluster.fork();
+    for (var i = 0; i < cpuNum; ++i) {
+        cluster.fork({doLog:!i});
     }
-    //클러스터가 죽으면
-    cluster.on('exit', (worker, code ,signal) => {
-        //클러스터를 다시 생성
-        cluster.fork();
-    });
 } else {
     require('./web');
 }
+
--- web.js
+++ web.js
@@ -1,14 +1,22 @@
+const cluster = require('cluster');
 const express = require('express');
 const app = express();
 
 const port = 8080;
+var doLog = (process.env.doLog==="true")?true:false;
+
+if (doLog) {
+    console.log("Product web.js");
+}
 
 app.use('/',require('./routers/index'));
 app.use('/user',require('./routers/user'));
-app.use('/media',require('./routers/midia'));
+app.use('/media',require('./routers/media'));
 app.use('/w',require('./routers/w'));
 app.use(require('./routers/error'));
 
 app.listen(8080, function () {
-  console.log(`Example app listening on port ${port}!`);
+    if (doLog) {
+        console.log(`Example app listening on port ${port}!`);
+    }
 });
