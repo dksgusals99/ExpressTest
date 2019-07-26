0ccf4c84eaa9fc99402d4459b0a5a415f5f7f652
unknown <dksgusals99@gmail.com>
2019-07-26 12:24:24 +0900

    1

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const app = express();
 const port = 8080;
 
 app.use('/',require('./routers/index'));
-app.use('/user',require('./routers/users'));
+app.use('/user',require('./routers/user'));
+app.use('/media',require('./routers/midia'));
+app.use('/w',require('./routers/w'));
 app.use(require('./routers/error'));
 
 app.listen(8080, function () {
