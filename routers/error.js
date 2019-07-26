ce81a64628119a2edd355bef0e6b21f05ce19c98
unknown <dksgusals99@gmail.com>
2019-07-26 12:25:43 +0900

    1

--- routers/error.js
+++ routers/error.js
@@ -4,12 +4,10 @@ var router = express.Router();
 router.use(function (req, res, next) {
     var path = req.headers.host + req.url
     res.status(404).send(`Sorry cant find ${path}!`);
-    //next();
 });
 
 router.use(function (err, req, res, next) {
     console.log(err);
-    //next();
 });
 
 module.exports = router;
--- routers/user.js
+++ routers/user.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 router.get('/', (req, res, next) => {
     res.send('Router Test User');
-    //next();
 });
 
 module.exports = router;
