bf05f9a1cd786203aa2276b9e3d3f69a890cddbf
HyunMin An <dksgusals99@gmail.com>
2019-07-28 19:04:43 +0900

    1

--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
 const settings = require('../setting.json')
 const wikiName = settings.name;
 const mainPage = settings.main_page;
@@ -7,8 +9,19 @@ const mainPage = settings.main_page;
 router.get('/', (req, res, next) => {
     res.redirect(`/${mainPage}`);
 });
+
 router.get(encodeURI(`/${mainPage}`), (req, res, next) => {
     res.render('index')
+});
+
+router.get('/:id', (req, res, next) => {
+    filePath = path.join(__dirname, '../views', req.params.id);
+    res.sendFile(filePath, (err) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(404);
+        }
+    });
 });
 
 module.exports = router;
